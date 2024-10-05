3cf6ffc19bf0d4dbe77b96a595d9ff1b543abad5
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2024-10-05 17:53:19 +0400

    Add files via upload

---
+++ module_7_2.py
@@ -0,0 +1,20 @@
+def custom_write(file_name, strings):
+    strings_positions = {}
+    file = open(file_name, 'a', encoding='utf-8')
+    for string in range(len(strings)):
+        tell_number = file.tell()
+        file.write(f'{strings[string]}\n')
+        strings_positions[(string + 1, tell_number)] = strings[string]
+    file.close()
+    return strings_positions
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+  print(elem)
